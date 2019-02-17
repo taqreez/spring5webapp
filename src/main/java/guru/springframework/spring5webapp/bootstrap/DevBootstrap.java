d391ad6ad870e51c319eb6d16b00fa15808824c9
taqreez <taqreez.ali@gmail.com>
2019-02-17 20:30:03 +0530

    Added publisher entity.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepositoriy;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepositoriy publisherRepositoriy;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepositoriy publisherRepositoriy) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepositoriy = publisherRepositoriy;
     }
 
     @Override
@@ -26,17 +30,21 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
         Author taqy = new Author("Taqy", "Ali");
-        Book b = new Book("Understanding web server", "1234", "McGraw Hills");
+        Publisher p = new Publisher("McGraw Hills", "Bangalore");
+        Book b = new Book("Understanding web server", "1234", p);
         taqy.getBooks().add(b);
         b.getAuthors().add(taqy);
 
+        publisherRepositoriy.save(p);
         authorRepository.save(taqy);
         bookRepository.save(b);
 
         Author areeb = new Author("Areeb", "Ali");
-        Book b2 = new Book("Wonders Ali family", "2344", "Marvel Publication");
+        Publisher p2 = new Publisher("Marvel Publication", "Delhi");
+        Book b2 = new Book("Wonders Ali family", "2344", p2);
         areeb.getBooks().add(b2);
 
+        publisherRepositoriy.save(p2);
         authorRepository.save(areeb);
         bookRepository.save(b2);
     }
